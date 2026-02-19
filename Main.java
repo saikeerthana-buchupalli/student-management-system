d92499cd02cc84824c8a0977dbcd46062afe67c9
Sai Keerthana Buchupalli <keerthana.22mic7127@vitapstudent.ac.in>
2026-02-19 21:16:24 +0530

    add delete student feature

--- Main.java
+++ Main.java
@@ -7,7 +7,8 @@ public static void main(String[] args){
     System.out.println("==== Student Management System ====");
     System.out.println("1. Add Student");
     System.out.println("2. View All Students");
-    System.out.println("3. Exit");
+    System.out.println("3. Delete Student");
+    System.out.println("4. Exit");
     System.out.print("Enter your choice: ");
     int choice = sc.nextInt();
     sc.nextLine();
